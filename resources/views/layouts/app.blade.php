99b1fe63eca43ce14954d3a9e08373f5221deda4
billal-lab <benyoubbillal@outlook.fr>
2021-05-04 20:32:46 +0200

    add some style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body  style="background-size: cover; background-image: url({{asset('images/bg.jpg')}})">
+<body  style="background-image: url({{asset('images/bg.jpg')}}); background-size: cover;">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
